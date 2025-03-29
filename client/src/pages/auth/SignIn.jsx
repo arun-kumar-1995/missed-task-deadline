b013287ff0c62f022609b0f8fc1c78ec0d5a3962
arun kumar <arun.devpro@gmail.com>
2025-03-29 16:27:42 +0530

    implended auth ui

--- client/src/pages/auth/SignIn.jsx
+++ client/src/pages/auth/SignIn.jsx
@@ -1,5 +1,69 @@
+import "./form.css";
+
+import { useEffect, useState, useCallback } from "react";
+import { useToast } from "../../contexts/ToastContext";
+import { Link } from "react-router-dom";
+
 const SignIn = () => {
-  return <h1>SignIn page</h1>;
+  const [loginInput, setLoginInput] = useState({ email: "", password: "" });
+  const [isLoading, setIsloading] = useState(false);
+  const toast = useToast();
+
+  const handleLoginInput = useCallback((e) => {
+    const { name, value } = e.target;
+    setLoginInput((prev) => ({ ...prev, [name]: value }));
+  }, []);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login-container form-container">
+      <div className="form-wrapper">
+        <form onSubmit={handleFormSubmit}>
+          <div className="form-info">
+            <h2>Sign In !</h2>
+            <p>
+              Are you new here?
+              <Link to="/account/sign-up">SignUp</Link>
+            </p>
+          </div>
+
+          <div className="form-section">
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                placeholder="Enter email"
+                value={loginInput.email}
+                name="email"
+                onChange={handleLoginInput}
+                required
+                autoComplete="on"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+
+              <input
+                type="password"
+                placeholder="Enter password"
+                value={loginInput.password}
+                name="password"
+                onChange={handleLoginInput}
+                required
+                autoComplete="off"
+              />
+            </div>
+            <button type="submit" disabled={isLoading}>
+              {isLoading ? "Checking..." : "Submit"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- client/src/pages/auth/SignUp.jsx
+++ client/src/pages/auth/SignUp.jsx
@@ -1,5 +1,98 @@
+import "./form.css";
+
+import { useEffect, useState, useCallback } from "react";
+import { useToast } from "../../contexts/ToastContext";
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
-  return <h1>Register page</h1>;
+  const [formInput, setformInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [isLoading, setIsloading] = useState(false);
+  const toast = useToast();
+
+  const handleFormInput = useCallback((e) => {
+    const { name, value } = e.target;
+    setformInput((prev) => ({ ...prev, [name]: value }));
+  }, []);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="signup-container form-container">
+      <div className="form-wrapper">
+        <form onSubmit={handleFormSubmit}>
+          <div className="form-info">
+            <h2>Sign Up !</h2>
+            <p>
+              Already have an account?
+              <Link to="/account/sign-in">SignIn</Link>
+            </p>
+          </div>
+
+          <div className="form-section">
+            <div className="form-group">
+              <label htmlFor="fullName">Enter name</label>
+              <input
+                type="text"
+                placeholder="First and last name"
+                value={formInput.name}
+                name="name"
+                onChange={handleFormInput}
+                required
+                autoComplete="on"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Enter email</label>
+              <input
+                type="text"
+                placeholder="Email-id@gmail.com"
+                value={formInput.email}
+                name="email"
+                onChange={handleFormInput}
+                required
+                autoComplete="on"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="min 8 character"
+                value={formInput.password}
+                name="password"
+                onChange={handleFormInput}
+                required
+                autoComplete="off"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="confirmPassword">Re-enter Password</label>
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                value={formInput.confirmPassword}
+                name="confirmPassword"
+                onChange={handleFormInput}
+                required
+                autoComplete="off"
+              />
+            </div>
+            <button type="submit" disabled={isLoading}>
+              {isLoading ? "Submitting..." : "Create account"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
