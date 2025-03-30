4d98af3aea0484026895caf1bc6f7f6a418922d8
arun kumar <arun.devpro@gmail.com>
2025-03-30 12:32:40 +0530

    task ui design update

--- client/src/pages/subpages/Tasks/Tasks.jsx
+++ client/src/pages/subpages/Tasks/Tasks.jsx
@@ -12,12 +12,21 @@ import { useState } from "react";
 import TaskModal from "../../../components/Modals/TaskModal";
 import EditTaskModal from "../../../components/Modals/EditTaskModal";
 import DeleteModal from "../../../components/Modals/DeleteModal";
+import userIcon from "../../../assets/dummyUserIcon.png";
+import { MdOutlineMoreHoriz } from "react-icons/md";
 
 const TaskPage = () => {
   const [isTaskModal, setTaskModal] = useState(false);
   const [isEditModal, setEditModal] = useState(false);
   const [isDeleteModal, setDeleteModal] = useState(false);
+  const [isTaskControl, setTaskControl] = useState(false);
 
+  const handleClose = () => {
+    setTaskModal(false);
+    setEditModal(false);
+    setDeleteModal(false);
+    setTaskControl(false);
+  };
   return (
     <div className="page-wrapper">
       <div className="page-header">
@@ -38,91 +47,109 @@ const TaskPage = () => {
         </div>
       </div>
 
-      <div className="task-wrapper">
-        <section className="section-todo">
+      <div className="task-sections">
+        <section className="section-todo --active">
           <div className="section-header">
-            <FaTasks size={20} />
+            <FaTasks size={18} />
             <h3>To do (05)</h3>
-          </div>
-          <div className="task-container">
-            <div className="task clr-todo">
-              <div className="task-header">
-                <h3>Optimize user controller</h3>
-                <div className="task-controls">
-                  <button
-                    className="icon-btn btn-edit"
-                    onClick={() => setEditModal(true)}
-                  >
-                    <MdOutlineEdit size={20} />
-                  </button>
-                  <button
-                    className="icon-btn btn-delete"
-                    onClick={() => setDeleteModal(true)}
-                  >
-                    <GoTrash size={18} />
-                  </button>
-                </div>
-              </div>
-              <div className="task-body">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisci elit, sed
-                  eiusmod tempor incidunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-            </div>
-            <div className="task">
-              <div className="task-header">
-                <h3>Optimize user controller</h3>
-                <div className="task-controls">
-                  <button
-                    className="icon-btn btn-edit"
-                    onClick={() => setEditModal(true)}
-                  >
-                    <MdOutlineEdit size={20} />
-                  </button>
-                  <button className="icon-btn btn-delete">
-                    <GoTrash size={18} />
-                  </button>
-                </div>
-              </div>
-              <div className="task-body">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisci elit, sed
-                  eiusmod tempor incidunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-            </div>
           </div>
         </section>
         <section className="section-inprogress">
           <div className="section-header">
-            <GrInProgress size={20} />
+            <GrInProgress size={18} />
             <h3>In Progress (08)</h3>
           </div>
         </section>
         <section className="section-todo">
           <div className="section-header">
-            <MdFileDownloadDone size={24} />
+            <MdFileDownloadDone size={20} />
             <h3>Done </h3>
           </div>
         </section>
       </div>
 
+      {/* Diplay task */}
+      <div className="task-container">
+        <div className="task-card">
+          <div className="task-header">
+            <h3>Design Branding</h3>
+            <button
+              type="button"
+              className="btn-task-control"
+              onClick={() => setTaskControl(true)}
+            >
+              <MdOutlineMoreHoriz size={20} />
+            </button>
+          </div>
+          <div className="task-card-body">
+            <span className="task-deadline">16 Oct</span>
+            <p>go through all the controllers of branding</p>
+            <div className="card-info">
+              <div className="task-card-user">
+                <img src={userIcon} alt="User Icon" className="user-profile" />
+                <img src={userIcon} alt="User Icon" className="user-profile" />
+                <img src={userIcon} alt="User Icon" className="user-profile" />
+              </div>
+              <button className="task-card-priority --priority-medium">
+                Medium
+              </button>
+            </div>
+          </div>
+
+          {isTaskControl && (
+            <div className="task-controls">
+              <div className="control" onClick={() => setEditModal(true)}>
+                <button className="icon-btn btn-edit">
+                  <MdOutlineEdit size={20} />
+                </button>
+                <span>Edit</span>
+              </div>
+
+              <div
+                className="control delete-task-control"
+                onClick={() => setDeleteModal(true)}
+              >
+                <button className="icon-btn btn-delete">
+                  <GoTrash size={18} />
+                </button>
+                <span>Delete</span>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="task-card">
+          <div className="task-header">
+            <h3>Optimize user controller</h3>
+            <div className="task-controls">
+              <button
+                className="icon-btn btn-edit"
+                onClick={() => setEditModal(true)}
+              >
+                <MdOutlineEdit size={20} />
+              </button>
+              <button className="icon-btn btn-delete">
+                <GoTrash size={18} />
+              </button>
+            </div>
+          </div>
+          <div className="task-body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
+              tempor incidunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+      </div>
       {/*Model stuffs  */}
-      {isTaskModal && (
-        <TaskModal isOpen={isTaskModal} onClose={() => setTaskModal(false)} />
-      )}
+      {isTaskModal && <TaskModal isOpen={isTaskModal} onClose={handleClose} />}
 
       {isEditModal && (
-        <EditTaskModal
-          isOpen={isEditModal}
-          onClose={() => setEditModal(false)}
-        />
+        <EditTaskModal isOpen={isEditModal} onClose={handleClose} />
       )}
 
       {isDeleteModal && (
         <DeleteModal
-          onClose={() => setDeleteModal(false)}
+          onClose={handleClose}
           data={{ id: "123", title: "Fix UI Bug" }}
         />
       )}
